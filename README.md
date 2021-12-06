366642f7a5fa65ee327fba5897d0e37252de890b
Sahil Thakur <sahilthakur376@gmail.com>
2021-12-07 02:14:30 +0530

    Added Tool
    Added Json to Xml tool

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# conversion-tool
-Conversion Tool - HostingSpell Internship
-
-This is conversion tool made using gatsby, During internship period with HostingSpell Organization
-
-Note: This repo is present in `hostingspell-dev` organization but is private repo set by organization.
-
-Tool Live link : https://deploy-preview-3--dev-tool-prod-hosting.netlify.app/
-
-Check my extra work done during internship here:
-https://github.com/hostingspell-dev
+# urban-engine
---
+++ src/pages/json-to-xml.js
@@ -0,0 +1,124 @@
+import React, { useRef, useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import * as JsXml from "js2xmlparser"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Sidebar from "../components/sidebar"
+
+
+const IndexPage = () => {
+    const myRef = useRef();
+    const [jsonInput, setJsonInput] = useState('');
+    const [isCopied, setIsCopied] = useState('');
+    const [jsonResult, setJsonResult] = useState(null);
+
+    function isValidFn(str) {
+        try {
+            JSON.parse(str);
+        }
+        catch {
+            return false;
+        }
+        return true;
+    }
+
+    const formatCallback = () => {
+        if (isValidFn(jsonInput)) {
+            let data = JsXml.parse("root", JSON.parse(jsonInput));
+            setJsonResult(data);
+        }
+        else {
+            setJsonResult("Check Json string");
+        }
+    }
+    const clearValues = () => {
+        setJsonInput('')
+        setJsonResult(null)
+    }
+    const uploadJson = (uploadedFile) => {
+        const fReader = new FileReader();
+
+        fReader.onloadend = () => {
+            try {
+                setJsonInput(fReader.result);
+            }
+            catch (e) {
+                setJsonResult("Check Json File");
+            }
+        }
+        if (uploadedFile !== undefined) {
+            fReader.readAsText(uploadedFile);
+        }
+    }
+
+    const handleCopy = () => {
+        let copyText = document.getElementById("resultTxt");
+        navigator.clipboard.writeText(copyText.innerHTML);
+        setIsCopied('copied');
+        setInterval(() => {
+            setIsCopied('copy')
+        }, 10000);
+    }
+
+    return (
+        <Layout>
+            <SEO title="Premium Tools" />
+
+            <div className="row no-gutters">
+                <div className="col-md-8 leftside">
+                    <div className="left-heading">
+                        <div className="container text-center">
+                            <h1> JSON to XML Converter</h1>
+                            <p className="lead">Conversion Tools By Premium Tools</p>
+                        </div>
+                        <div className="container">
+                            <div className="container my-2 row justify-content-center card-container card shadow card-body" >
+                                <form id="converterForm" >
+                                    <div className="pretifier-container">
+                                        <div className="field-content">
+                                            <div className="field-view">
+                                                <h5 className="res-title">JSON Data / String</h5>
+                                                <div className="controls">
+                                                    <input type="file" className="hidden"
+                                                        multiple={false}
+                                                        accept=".json,application/json"
+                                                        onChange={(et) => uploadJson(et.target.files[0])}
+                                                        ref={myRef}
+                                                    />
+                                                    <textarea name="field" id="field" data-gramm_editor="false" spellCheck="false" placeholder="{}" rows="5" required value={jsonInput} onChange={(e) => setJsonInput(e.target.value)}></textarea>
+                                                    <div className="control-btns">
+                                                        <button type="button" title="Load Json" name="browse" aria-label="Load Json" className="btn" onClick={(evt) => {
+                                                            evt.preventDefault();
+                                                            myRef.current.click();
+                                                        }}><FontAwesomeIcon className="icon" icon={["fas", "folder-open"]} /></button>
+                                                        <button type="button" title="Clear" name="clear" aria-label="Clear" className="btn" onClick={() => clearValues()}><FontAwesomeIcon className="icon" icon={["fas", "trash-alt"]} /></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="btn-container">
+                                            <button type="button" title="Convert to XML" className="btn btn-primary mr-2" onClick={() => formatCallback()}>Convert</button>
+                                            <button type="button" title="Copy" className="btn btn-primary mr-2" name="copy" aria-label="Copy Json to Clipboard" onClick={() => handleCopy()}><FontAwesomeIcon icon={["fas", "copy"]} /> {isCopied === "copied" ? " Copied" : " Copy"}</button>
+                                        </div>
+                                        {
+                                            jsonResult === null || jsonResult === "" || jsonResult === undefined ? " " :
+                                                <div className="result-container">
+                                                    <h5 className="res-title">XML Formatted Data</h5>
+                                                    <pre className="result-view" id="resultTxt">
+                                                        {jsonResult}
+                                                    </pre>
+                                                </div>
+                                        }
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Sidebar />
+            </div>
+        </Layout>
+    )
+}
+export default IndexPage
