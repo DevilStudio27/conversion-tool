eb990e7c5d9b95971fd23ec374fcb0a0aa0c6269
Sahil Thakur <sahilthakur376@gmail.com>
2021-12-17 11:17:03 +0530

    Added Pie Chart Tool
    Added Pie Chart Tool
    Added Cool Font

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -12,22 +12,22 @@ const sidebar = () => (
                     <div className="card-inner-body">
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="watt-to-amp"><h6 >Watt To Ampere</h6></a>
+                            <a href="/watt-to-amp"><h6 >Watt To Ampere</h6></a>
                         </div>
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="watt-to-vlt"><h6 >Watt To Volt</h6></a>
+                            <a href="/watt-to-vlt"><h6 >Watt To Volt</h6></a>
                         </div>
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="watt-to-va"><h6 >Watt To VA</h6></a>
+                            <a href="/watt-to-va"><h6 >Watt To VA</h6></a>
                         </div>
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="watt-to-kva"><h6 >Watt To kVA</h6></a>
+                            <a href="/watt-to-kva"><h6 >Watt To kVA</h6></a>
                         </div>
                     </div>
                 </div>
@@ -43,29 +43,29 @@ const sidebar = () => (
                     <div className="card-inner-body">
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="amp-to-kw"><h6 >Ampere To kW</h6></a>
+                            <a href="/amp-to-kw"><h6 >Ampere To kW</h6></a>
                         </div>
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="amp-to-kva"><h6 >Ampere To kVA</h6></a>
-                        </div>
-
-                        <hr className="line" />
-                        <div className="card-inner-body-1">
-                            <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="amp-to-va"><h6 >Ampere To VA</h6></a>
+                            <a href="/amp-to-kva"><h6 >Ampere To kVA</h6></a>
                         </div>
 
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="amp-to-vlt"><h6 >Ampere To Volt</h6></a>
+                            <a href="/amp-to-va"><h6 >Ampere To VA</h6></a>
+                        </div>
+
+                        <hr className="line" />
+                        <div className="card-inner-body-1">
+                            <i className="fa fa-check side-item" aria-hidden="true"></i>
+                            <a href="/amp-to-vlt"><h6 >Ampere To Volt</h6></a>
                         </div>
                         <hr className="line" />
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="amp-to-watt"><h6 >Ampere To Watt</h6></a>
+                            <a href="/amp-to-watt"><h6 >Ampere To Watt</h6></a>
                         </div>
                     </div>
                 </div>
@@ -80,23 +80,28 @@ const sidebar = () => (
                     <div className="card-inner-body">
                         <div className="card-inner-body-1">
                             <i className="fa fa-check side-item" aria-hidden="true"></i>
-                            <a href="electricity-bill"><h6 >Electricity Bill</h6></a>
+                            <a href="/electricity-bill"><h6 >Electricity Bill</h6></a>
                         </div>
                     </div>
                     <hr className="line" />
                     <div className="card-inner-body-1">
                         <i className="fa fa-check side-item" aria-hidden="true"></i>
-                        <a href="json-validator"><h6 >Json Validator</h6></a>
+                        <a href="/json-validator"><h6 >Json Validator</h6></a>
                     </div>
                     <hr className="line" />
                     <div className="card-inner-body-1">
                         <i className="fa fa-check side-item" aria-hidden="true"></i>
-                        <a href="json-pretifier"><h6 >Json Pretifier</h6></a>
+                        <a href="/json-pretifier"><h6 >Json Pretifier</h6></a>
                     </div>
                     <hr className="line" />
                     <div className="card-inner-body-1">
                         <i className="fa fa-check side-item" aria-hidden="true"></i>
-                        <a href="json-to-xml"><h6 >JSON To XML</h6></a>
+                        <a href="/json-to-xml"><h6 >JSON To XML</h6></a>
+                    </div>
+                    <hr className="line" />
+                    <div className="card-inner-body-1">
+                        <i className="fa fa-check side-item" aria-hidden="true"></i>
+                        <a href="/pie-chart"><h6 >Pie Charts</h6></a>
                     </div>
                 </div>
             </div>
---
+++ src/pages/pie-chart.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react"
+import { PieChart, Pie, Tooltip, Cell, Legend } from 'recharts';
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Sidebar from "../components/sidebar"
+
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x + 2.5} y={y + 1.5} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
+const IndexPage = () => {
+    const [loader, setLoad] = useState(true);
+    const [mTitle, setTitle] = useState('');
+    const [mLabel, setLabel] = useState('');
+    const [mValue, setValue] = useState('');
+    const [pie_data, setPieData] = useState([null]);
+
+    const formatCallback = () => {
+        setPieData([null]);
+        setLoad(false);
+        let temp = String(mLabel).split(" ");
+        let temp1 = String(mValue).split(" ");
+        for (var i = 0; i < temp.length && i < temp1.length; i++) {
+            let name = temp[i];
+            let score = temp1[i];
+            let query = { "name": String(name), "value": Number(score) }
+            // setPieData(pie_data => pie_data.push(query));
+            setPieData(pie_data => [...pie_data, query]);
+        };
+        // console.log(pie_data);
+
+    }
+    const clearValues = () => {
+        setLoad(true)
+        setTitle('')
+        setLabel('')
+        setValue('')
+        setPieData([null]);
+    }
+
+    return (
+        <Layout>
+            <SEO title="Premium Tools" />
+
+            <div className="row no-gutters">
+                <div className="col-md-8 leftside">
+                    <div className="left-heading">
+                        <div className="container text-center">
+                            <h1>Pie Chart Maker</h1>
+                            <p className="lead">Conversion Tools By Premium Tools</p>
+                        </div>
+                        <div className="container">
+                            <div className="container my-2 row justify-content-center card-container card shadow card-body" >
+                                <form id="converterForm" >
+                                    <div className="pretifier-container">
+                                        <div className="field-content">
+                                            <div className="input-controls">
+                                                <label htmlFor="tabin">Enter Title</label>
+                                                <input type="text" name="tabin" id="tabin" required value={mTitle} onChange={e => setTitle(e.target.value)}></input>
+                                            </div>
+                                            <div className="input-controls">
+                                                <label htmlFor="tabin">Enter Data Labels</label>
+                                                <input type="text" name="tabin" id="tabin" required value={mLabel} multiple={true} onChange={e => setLabel(e.target.value)}></input>
+                                            </div>
+                                            <div className="input-controls">
+                                                <label htmlFor="tabin">Enter Data Values</label>
+                                                <input type="text" name="tabin" id="tabin" required value={mValue} multiple={true} onChange={e => setValue(e.target.value)}></input>
+                                            </div>
+                                        </div>
+                                        <div className="btn-container">
+                                            <button type="button" title="Show Pie Chart" className="btn btn-primary mr-2" onClick={() => formatCallback()}>Show Pie Chart</button>
+                                            <button type="button" title="Clear" name="clear" aria-label="Clear" className="btn btn-secondary" onClick={() => clearValues()}>Clear</button>
+                                        </div>
+
+                                        <div className="pie-container">
+                                            {
+                                                !loader ?
+                                                    <div className="pie-content">
+                                                        <h3 className="pie_title">{mTitle}</h3>
+                                                        <PieChart width={400} height={400} key={`p_c${pie_data.length}`}>
+                                                            <Pie
+                                                                data={pie_data}
+                                                                key={`p_${pie_data.length}`}
+                                                                cx="50%"
+                                                                cy="50%"
+                                                                labelLine={false}
+                                                                label={renderCustomizedLabel}
+                                                                outerRadius={100}
+                                                                fill="#8884d8"
+                                                                dataKey="value"
+                                                            >
+                                                                {pie_data.map((entry, index) => (
+                                                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                                                ))}
+                                                            </Pie>
+                                                            <Tooltip wrapperStyle={{ fontSize: '14px', padding: 0 }} />
+                                                            <Legend />
+                                                        </PieChart>
+                                                    </div> : " "
+
+                                            }
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Sidebar />
+            </div>
+        </Layout>
+    )
+}
+export default IndexPage
